b441d720c2f5e8b62b065699312cd625b59e71a4
Seren <SerenTera@users.noreply.github.com>
2017-08-21 19:43:49 +0800

    Initial Commit

---
+++ config.js
@@ -0,0 +1,53 @@
+//This file contains various configs for all the avaliable notifications
+//enable = whether this notification is enabled or not (Use true or false to disable)
+//afknotify = whether to use afknotification or to notify everytime event occurs. true to use notifyafk, false to use notify(will notify everytime event occurs)
+//timeout = time out to use in afknotification in milliseconds.(use numbers only) (1sec=1000milsec)
+
+//Note: messages that are displayed are in index.js, under the hooks, in order to not over complicate the module. and also im too noob lul
+//In index.js, edit the messages on your own. use \n for line breaks.
+
+module.exports = {
+	
+	ims:{ //IMS Ready notification: Notify if you have been matched to an instance via ims. 
+		enable:true,
+		afknotify:false, //You can set afknotify to false to notify you everytime if you always afk while imsing like me :D
+		timeout:15000
+	},
+	
+	whisper:{ //Notify whenever someone whispers you
+		enable:true,
+		afknotify:true,
+		timeout:60000
+	},
+	
+	incombat:{ //Notify when you are suddenly in combat
+		enable:false,
+		afknotify:true,
+		timeout:120000
+	},
+	
+	trade:{ //notify when someone trades you
+		enable:true,
+		afknotify:true,
+		timeout:60000
+	},
+	
+	duel:{ //notify when someone ask for duels
+		enable:true,
+		afknotify:true,
+		timeout:60000
+	},
+	
+	party:{ //notify when someone parties you
+		enable:true,
+		afknotify:true,
+		timeout:60000
+	},
+	
+	test:{ //debugging testing function
+		enable:false,
+		afknotify:true,
+		timeout:10000
+	}
+	
+}
---
+++ index.js
@@ -0,0 +1,119 @@
+const Command = require('command'),
+	  Notifier = require('tera-notifier'),
+	  config = require('./config')
+	  
+//Defaults
+let enabled=true,				//Default enabling of module.
+	soundId='Notification.IM'	//Use true for default windows notification sound. Or use false for silence. For more, read: http://msdn.microsoft.com/en-us/library/windows/apps/hh761492.aspx
+
+	
+	
+module.exports = function afknotify(dispatch) {
+	const command = Command(dispatch),
+		  notifier = new Notifier(dispatch)
+		  
+	let cid
+	
+	/////Command
+	command.add('afk', () => {
+		enabled=!enabled
+		command.message( enabled ? '(AFK Notify) Enabled' : '(AFK Notify) Disabled')
+	})
+	
+	/////Dispatches
+	dispatch.hook('S_LOGIN', 2, event => {
+		cid = event.cid
+	})
+	
+	
+	//Instance Match (ims)
+	dispatch.hook('S_FIN_INTER_PARTY_MATCH', 'raw', () => { 
+		parseconfig({ 
+		
+			configname: 'ims',
+			message:'[Instance Match] Ready'
+		})
+	})
+	
+	//Whispers (whisper)
+	dispatch.hook('S_WHISPER', 1, event => { 
+		parseconfig({
+			
+			configname:'whisper',
+			message:'[Whisper] '+event.author+'\n'+event.message
+		})
+	})
+	
+	//Trade and Duel (trade,duel)
+	dispatch.hook('S_REQUEST_CONTRACT', 1, event => {
+		switch(event.type) {
+			case 3: //trade
+				parseconfig({
+					
+					configname:'trade',
+					message:'[Request Trade]\n'+event.senderName+' wants to trade'
+				})
+				break
+			case 11: //Duel
+				parseconfig({
+					
+					configname:'duel',
+					message:'[Request Duel]\n'+event.senderName+' wants to duel'
+				})
+				break
+		}
+	})
+	
+	//Party Request (party)
+	dispatch.hook('S_BEGIN_THROUGH_ARBITER_CONTRACT', 1, event => {
+		if(event.type===4) parseconfig({
+			
+			configname:'party',
+			message:'[Request Party]\n'+event.sender+' wants to party'
+		})
+	})
+
+	//Combat status changed (incombat)
+	dispatch.hook('S_USER_STATUS', 1, event => {
+		if(event.target.equals(cid) && event.status === 1) parseconfig({
+	
+			configname:'incombat',
+			message:'[Combat]\nYou are in combat'
+		})
+	})
+	
+	//Test (test)
+	/*dispatch.hook('C_CHAT', 1, event => {
+		parseconfig({
+			configname:'test',
+			message:'[Message]\n'+event.message
+		})
+	})*/
+	
+	
+	/////Functions
+	function parseconfig(set) {
+		if(config[set.configname].enable && enabled) {
+			
+			config[set.configname].afknotify ? notificationafk(set.message,config[set.configname].timeout): notification(set.message)
+		}
+	}
+	
+	function notification(msg) {
+		notifier.notify({
+			title: 'AFK Notification',
+			message: msg,
+			wait:false, 
+			sound:soundId, 
+		})
+	}
+		
+	function notificationafk(msg,timeout) { //timeout in milsec
+		notifier.notifyafk({
+			title: 'AFK Notification',
+			message: msg,
+			wait:false, 
+			sound:soundId, 
+		},timeout)
+	}
+}
